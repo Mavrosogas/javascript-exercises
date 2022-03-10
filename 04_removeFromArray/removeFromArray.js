691af1443a3b9babce1b1724740e128d0b0c0b58
mavrosogas <vmavrouleas@live.co.uk>
2022-03-10 19:10:23 +0200

    ok

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
 const removeFromArray = function() {
-
+    let args = Array.from(arguments);
+    let arr = args[0];
+    args.shift();
+    let undisirable = args;
+    let result = new Array;
+    for (let i=0 ; i < undisirable.length ; i++){
+        result = arr.filter(num => num !== undisirable[i]);
+        arr = result;
+    }
+    return result;
 };
-
-// Do not edit below this line
 module.exports = removeFromArray;
