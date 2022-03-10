e122344e058dc127d52e686558a41a2722c5aa55
mavrosogas <vmavrouleas@live.co.uk>
2022-03-10 04:46:34 +0200

    ok

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    const tmpArr = inputString.split('');
+    let revArr = new Array();
+    for (let i = 0 ; i <= tmpArr.length ; i++){
+        revArr[i] = tmpArr[tmpArr.length - i];
+    }
+    let resultString = revArr.join('');
+    return resultString;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
