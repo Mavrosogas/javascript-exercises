eee8c3f2eff5704612aaba6f54b31bed2b5c3a77
mavrosogas <vmavrouleas@live.co.uk>
2022-03-10 03:58:37 +0200

    ok

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word,numberOfTimes) {
+    let sumString = word;
+    let result = word;
+    for (let i = 1 ; i < numberOfTimes ; i++ ){
+        sumString = result;
+        result = sumString.concat(word);
+    }
+     if (numberOfTimes > 0 && word){
+        return result;
+     } else if (numberOfTimes == 0 || word == '') {
+        return "";
+     } else {
+        return "ERROR";
+     }
 };
 
 // Do not edit below this line
