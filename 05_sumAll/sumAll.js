1c1b2ac8e5a10279586560d74869a827a4e9563c
mavrosogas <vmavrouleas@live.co.uk>
2022-03-11 09:45:53 +0200

    ok

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(bottomNumber,topNumber) {
+    let sum=0;
+    if (bottomNumber>topNumber){
+        let tmp = topNumber;
+        topNumber = bottomNumber;
+        bottomNumber = tmp;
+    }
+    if ((bottomNumber <= 0 || typeof bottomNumber != "number") || (topNumber <= 0 || typeof topNumber != "number")){
+        return 'ERROR';
+    }
+    for (i=bottomNumber; i<=topNumber ; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
