23770dd4ec9795e590a0b5b915cc4071582d1e93
mavrosogas <vmavrouleas@live.co.uk>
2022-03-12 00:44:33 +0200

    ok

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const ftoc = function() {
-
+  return Math.round(((arguments[0]-32)*5/9)*10)/10;
 };
 
 const ctof = function() {
-
+  return Math.round(((arguments[0]*9/5)+32)*10)/10;
 };
 
 // Do not edit below this line
